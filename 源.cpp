8c6da0627642fd060ff56da88063fe354de76628
protish <3295441197@qq.com>
2024-09-04 09:55:03 +0800

    Update 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-using namespace std;
-int main()
-{int age;
-cin >> age;
-if (age < 18)
-	printf("未成年\n");
-else if (age >= 18 && age < 35)
-	printf("青年\n");
-else if (age >= 35 && age < 60)
-	printf("壮年\n");
-else
-printf("老年\n");
-return 0;
-}
+
