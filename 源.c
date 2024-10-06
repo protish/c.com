b4d001b53dbff6f16f629de8bd4ab915ca3cf1ef
protish <3295441197@qq.com>
2024-10-06 22:38:33 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+#define r 6371;
+#define san 3.1415926;
+double hav(double d)
+{
+	double b;
+	b = (1 - cos(d)) / 2;
+	return b;
+}
+double lp(double F, double G, double H, double J)
+{
+	double i, Q, W, E, R;
+	Q = F * san / 180;
+	W = G * san / 180;
+	E = H * san / 180;
+	R = J * san / 180;
+	i = hav(E - Q) + cos(E) * cos(Q) * hav(R - W);
+	return i;
+}
 int main()
 {
-import random
-
-	int num = 0;
-	num = random.randint(1, 100)
-		count = 0;
-	while (count < 7)
-	{
-		count += 1;
-		guess = int(input("请猜一个数："))
-			if (guess > num)
-			{
-				printf("大了\n");
-				countine;
-			}
-		elif(guess == num)
-		{
-			printf("对了\n");
-			break;
-		}
-			else:
-		printf("小了\n");
-	else:
-	printf("输入次数过多\n");
-	}
+	double f, g, h, j, m, z, x;
+	scanf("%lf %lf %lf %lf", &f, &g, &h, &j);
+	m = lp(f, g, h, j);
+	z = acos(1 - 2 * m);
+	x = z * r;
+	printf("%.4lfkm\n", x);
 	return 0;
 }
